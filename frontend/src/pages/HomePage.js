2937f3222d5a4d031e87d27dc6f7b213e264dd11
Rtur2003 <hasannarthurrr@gmail.com>
2025-08-16 10:49:15 +0300

    s

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import '../styles/main.css';
 import { generateComment, postCommentToYouTube, getHistory } from '../services/api';
 import CommentForm from '../components/CommentForm';
